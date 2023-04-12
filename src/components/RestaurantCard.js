3c0e33136ede866da3ed4db34c8b366b3831cf45
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-12 15:26:34 +0530

    q

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,18 +1,18 @@
 const RestaurantCard = (props) => {
-    const {resName, cuisine} = props;
+    const {resData} = props;
     
     
-    // console.log('props', props);
+
     return(    
     <div className="res-card" style={{
         backgroundColor:"yellow",
     }}>
         <img className='res-logo'
-        src="https://filmymantra.com/wp-content/uploads/2019/01/maggi1.jpg" alt="" />
-        <h3>{resName}</h3>
-        <h4>{cuisine}</h4>
-        <h4>4.4 Stars</h4>
-        <h4>38 minutes</h4>
+        src={resData.imageSrc} alt="" />
+        <h3>{resData.resName}</h3>
+        <h4>{resData.cuisine}</h4>
+        <h4>{resData.stars}</h4>
+
     </div>
 )}
 
