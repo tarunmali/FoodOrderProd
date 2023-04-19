1b3c8a34ffda2955e6661d6ec58b025029508384
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-19 11:10:53 +0530

    two types of searches

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,29 +5,47 @@ import {RESTAURANT_LIST} from "../utils_common_config/constants";
 const Body = () => 
 {
 
-
-    const [searchText, setSearchText] = useState("");
+    
+    const [searchState1, setSearchState1] = useState("Button daba ke search karne wala")
+    const [searchState2, setSearchState2] = useState("A bLAZINGLYfAST search experience")
+    const [searchText1, setSearchText1] = useState("");
+    const [searchText2, setSearchText2] = useState("");
     const [listOfRestaurants, setListOfRestaurants] = useState(RESTAURANT_LIST);
-    const [searchState, setSearchState] = useState("A bLAZINGLYfAST search experience")
     return (
         
         <div className="body">
-            <div className="search-container">
-                <h3>{searchState}</h3>
-                <input type="text" className="search-input" placeholder="Search" value={searchText}
-                //this e is provided by react, also avaialable in js 
+            <div className="search-container-1">
+                <h3>{searchState1}</h3>
+                <input type="text" className="search-input" placeholder="Search" value={searchText1}
+                //this e is provided by  js 
                 onChange={(e)=>{
-                    setSearchText(e.target.value);
+                    setSearchText1(e.target.value);
+                }}
+                />
+                <button className="search-btn" onClick={()=>{
+                    setSearchState1("Search started...")
+                    setListOfRestaurants(RESTAURANT_LIST.filter(
+                        (restaurant)=> restaurant.resName.toLowerCase().includes(searchText1.toLowerCase())
+                    ))
+                }}>Search</button>
+            </div>
+
+            <div className="search-container-2">
+                <h3>{searchState2}</h3>
+                <input type="text" className="search-input" placeholder="Search" value={searchText2}
+                onChange={(e)=>{
+                    setSearchText2(e.target.value);
                     setListOfRestaurants(RESTAURANT_LIST.filter(
                         (restaurant)=> restaurant.resName.toLowerCase().includes(e.target.value.toLowerCase())
                     ))
                 }}
                 />
                 <button className="search-btn" onClick={()=>{
-                    setSearchState("Search started...")
+                    setSearchState2("Why click anyway when results are bLAZINGLYFAST")
 
                 }}>Search</button>
             </div>
+
             <div className='filter'>
             <button className="filter-btn" 
             onClick={()=>{
