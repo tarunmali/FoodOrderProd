a8f008029f6ca2fb5a6bf852d8547c388d94491e
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-19 10:57:35 +0530

    search working

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,32 +1,13 @@
 import {useState} from "react";
 import RestaurantCard from "./RestaurantCard";
+import {RESTAURANT_LIST} from "../utils_common_config/constants";
 
 const Body = () => 
 {
 
 
     const [searchText, setSearchText] = useState("");
-    const [listOfRestaurants, setListOfRestaurants] = useState([
-        { resName:"Meghana Food", 
-    cuisine:"Biryani, North Indian",
-    stars:4.4,
-    imageSrc: "https://filmymantra.com/wp-content/uploads/2019/01/maggi1.jpg"
-    },
-    {   
-        resName:"KFC", 
-        cuisine:"Fast food",
-        stars:3.8,
-        imageSrc: "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/l/f/p4-15578295855cda97d139913.jpg?tr=tr:n-xlarge"
-    },
-    {   
-        resName:"Mc Donald", 
-        cuisine:"Fast food",
-        stars:4.9,
-        imageSrc: "https://content.jdmagicbox.com/comp/ahmedabad/y6/079pxx79.xx79.130813113637.l5y6/catalogue/mcdonald-s-navrangpura-ahmedabad-fast-food-ibxnfmcmd6.jpg?clr="
-
-    },
-    
-]);
+    const [listOfRestaurants, setListOfRestaurants] = useState(RESTAURANT_LIST);
     const [searchState, setSearchState] = useState("A bLAZINGLYfAST search experience")
     return (
         
@@ -37,22 +18,23 @@ const Body = () =>
                 //this e is provided by react, also avaialable in js 
                 onChange={(e)=>{
                     setSearchText(e.target.value);
+                    setListOfRestaurants(RESTAURANT_LIST.filter(
+                        (restaurant)=> restaurant.resName.toLowerCase().includes(e.target.value.toLowerCase())
+                    ))
                 }}
                 />
                 <button className="search-btn" onClick={()=>{
                     setSearchState("Search started...")
+
                 }}>Search</button>
             </div>
             <div className='filter'>
-            {/* //call back function, function inside {} */}
             <button className="filter-btn" 
             onClick={()=>{
-                //filter logic
-                let updatedListOfRestaurants=listOfRestaurants.filter(
+                setListOfRestaurants(listOfRestaurants.filter(
                     (restaurant)=> restaurant.stars>4
-                );
-                setListOfRestaurants(updatedListOfRestaurants)
-                // console.log(listOfRestaurants);
+                ))
+
             }}
             > Top Rated Restaurant </button>
             </div>
--- src/utils_common_config/constants.js
+++ src/utils_common_config/constants.js
@@ -2,3 +2,24 @@
 export const LOGO_URL=
 "https://blog.ipleaders.in/wp-content/uploads/2019/11/foodmitho.jpg";
 
+export const RESTAURANT_LIST = [
+    { resName:"Meghana Food", 
+cuisine:"Biryani, North Indian",
+stars:4.4,
+imageSrc: "https://filmymantra.com/wp-content/uploads/2019/01/maggi1.jpg"
+},
+{   
+    resName:"KFC", 
+    cuisine:"Fast food",
+    stars:3.8,
+    imageSrc: "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/4/l/f/p4-15578295855cda97d139913.jpg?tr=tr:n-xlarge"
+},
+{   
+    resName:"Mc Donald", 
+    cuisine:"Fast food",
+    stars:4.9,
+    imageSrc: "https://content.jdmagicbox.com/comp/ahmedabad/y6/079pxx79.xx79.130813113637.l5y6/catalogue/mcdonald-s-navrangpura-ahmedabad-fast-food-ibxnfmcmd6.jpg?clr="
+
+},
+
+]
