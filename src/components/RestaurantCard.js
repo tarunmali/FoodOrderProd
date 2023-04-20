180c6383b605e2f633f9b81cadb4600cebeed14f
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-20 23:53:36 +0530

    q

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,19 +1,27 @@
 import {IMG_CDN_URL} from "../utils_common_config/constants";
 
+import { Link } from "react-router-dom";
+
 const RestaurantCard = (props) => {
     // console.log(props);
 const {resData}=props;
     // console.log(resData.data);
     return(    
+
+
     <div className="res-card" >
+        <Link to= {"/restaurant/"+resData.data.id} >
         {/* <img 
         src={} alt="" /> */}
         <img className='res-logo' src={IMG_CDN_URL+resData.data.cloudinaryImageId} alt="" />
         <h3>{resData.data.name}</h3>
         <h4>{resData.data.cuisines.toString()}</h4>
         {/* <h4>{resData.stars}</h4> */}
-
+        </Link>
     </div>
+
+
+
 )}
 
 export default RestaurantCard;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,5 @@
 import {IMG_CDN_URL} from "../utils_common_config/constants";
+import Shimmer from "./Shimmer";
 
 
 import { useParams } from "react-router-dom"
@@ -10,30 +11,35 @@ import { IMG_CDN_URL } from "../utils_common_config/constants";
 const RestaurantMenu = ({ restaurant }) => {
 
     const params=useParams();
-    const [restaurantInfo,setRestaurantInfo]=useState({});
+    const [restaurantInfo,setRestaurantInfo]=useState(null);
 
     useEffect(()=>{
         getRestaurantInfo();
     },[]);
 
     async function getRestaurantInfo(){
-        const data= await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId=176437")
+        const data= await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId="+params.resId)
         const json= await data.json();
+        setRestaurantInfo(json.data);
         // console.log(json);
-        console.log(json.data.cards[0].card.card.info);
-        setRestaurantInfo(json.data.cards[0].card.card.info);
+        // console.log(json.data.cards[0].card.card.info);
     }
 
 
+    if(!restaurantInfo){
+        return <Shimmer/>
+    }
+
     return <div className="restaurant-menu">
         <h1>{`Restaurant id: ${params.resId}`}</h1>
-        <h2>{restaurantInfo.name}</h2>
-        <img src={IMG_CDN_URL+restaurantInfo.cloudinaryImageId} alt="" />
-        <h3>{restaurantInfo.area}</h3>
-        <h3>{restaurantInfo.avgRating} stars</h3>
-        <h3>{restaurantInfo.city}</h3>
-        <h3>{restaurantInfo.costForTwoMessage}</h3>
-
+        <h2>{restaurantInfo?.cards[0]?.card?.card?.info?.name}</h2>
+        <h3>{restaurantInfo?.cards[0]?.card?.card?.info?.area}</h3>
+        <img src={IMG_CDN_URL+restaurantInfo?.cards[0]?.card?.card?.info?.cloudinaryImageId} alt="" />
+        <h3>{restaurantInfo?.cards[0]?.card?.card?.info?.avgRating} stars</h3>
+        <h3>{restaurantInfo?.cards[0]?.card?.card?.info?.city}</h3>
+        <h3>{restaurantInfo?.cards[0]?.card?.card?.info?.costForTwoMessage}</h3> 
+         {/* {console.log(restaurantInfo)} */}
+        {console.log(restaurantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.find(x => x?.card?.card?.title=="Recommended"))}
 
     </div> 
 }
