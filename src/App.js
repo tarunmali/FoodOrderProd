56f118c537966ecdbb6f0a1850cae260cf2ba53b
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-20 09:52:24 +0530

    q

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import About from './components/About';
+import Error from './components/Error';
+
 
 const App = () =>  (
     <div class="app">
@@ -12,6 +17,19 @@ const App = () =>  (
     </div>
 )
 
+const appRouter=createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <Error/>
+    },
+    {
+       path: "/about",
+       element: <About/> 
+    }
+]);
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+
+root.render(<RouterProvider router={appRouter}/>);
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ const Shimmer = () => {
     return(
         <>
         <div className='res-container'>
-            {Array(20).fill("").map(e=><div className="shimmer-card"></div>)}
+            {Array(20).fill("").map((e,index)=><div index={index} className="shimmer-card"></div>)}
             <div className="shimmer-card"></div>
     
         </div> 
