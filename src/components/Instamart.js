e3380b956486a632cc16e83d3921efcd63bfb2b2
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-22 21:43:46 +0530

    not functional accordian

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -4,8 +4,6 @@ import {useState} from 'react';
 
 
 const Section =({title,description,isVisible,setIsVisible})=>{
-        
-
         return (<div className="border border-black p-2 m-2">
                 <h3 className="font-bold text-xl">{title}</h3>
 
@@ -17,17 +15,35 @@ const Section =({title,description,isVisible,setIsVisible})=>{
 
 
 const Instamart = () => {
-        const [sectionConfig,setsectionConfig]=useState({
-                showAbout:false,
-                showTeam: false,
-                showCareers:false
-        });
+
+        const [visibleSection,setVisibleSection]=useState("about");
+
 
         return(<div>
                 <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
-                <Section setIsVisible={()=>setsectionConfig({showAbout:true,showTeam:false,showCareers:false})} isVisible={sectionConfig.showAbout} title={"About Instamart"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}/>
-                <Section setIsVisible={()=>setsectionConfig({showAbout:false,showTeam:true,showCareers:false})} isVisible={sectionConfig.showTeam} title={"Team Instamart"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}/>
-                <Section setIsVisible={()=>setsectionConfig({showAbout:false,showTeam:false,showCareers:true})} isVisible={sectionConfig.showCareers} title={"Careers"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}/>
+                <Section 
+                setIsVisible={()=>setVisibleSection("about")}
+                title={"About Instamart"} 
+                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}
+                isVisible={visibleSection==="about"}
+                />
+
+                <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+                <Section 
+                setIsVisible={()=>setVisibleSection("team")}
+                title={"Team Instamart"} 
+                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}
+                isVisible={visibleSection==="team"}
+                />
+
+                <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+                <Section 
+                setIsVisible={()=>setVisibleSection("careers")}
+                title={"Careers"} 
+                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quas porro voluptatibus atque minima modi neque accusantium velit, iure dignissimos beatae tempore sit odit quisquam similique maxime nisi vero sed ad nostrum. Sunt incidunt quo saepe? Sed, fugit quos. Dicta culpa porro expedita cupiditate deleniti soluta deserunt iste beatae, obcaecati minima incidunt nemo ipsa est earum nihil suscipit quam iusto quos distinctio reprehenderit dolorem accusamus quasi fugiat? Cum voluptatem ducimus, labore perferendis accusantium quo dolore, delectus natus fuga quae similique incidunt ad. Aliquam animi non voluptatum, hic commodi provident fugit quam quasi nam consequatur inventore ducimus eius in, libero saepe."}
+                isVisible={visibleSection==="careers"}
+                />
+
         </div>)
 }
 
